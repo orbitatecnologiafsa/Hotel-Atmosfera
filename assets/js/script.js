cbefda98e70ccc97ca6f3c2c746292f254db7e18
Vitel <vitelrfs@outlook.com>
2024-06-10 11:18:38 -0300

    validacao de cpf e campo de nome adicionado

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,13 +48,66 @@ const form = document.getElementById('form');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
+
+    const cpf = document.getElementById('cpf').value;
+
+    if(!validarCpf(cpf)){
+        alert('CPF inválido');
+        return;
+    }
+    const nome = document.getElementById('nome').value;
     const local = document.getElementById('local').value;
     const data = document.getElementById('data').value;
-    const cpf = document.getElementById('cpf').value;
+    
     const dados = {
+        nome,
         local,
         data,
         cpf
     };
     window.location.href = './foto.html';
 });
+
+
+// Validar CPF
+
+function validarCpf(cpf){
+
+    let Soma = 0;
+    let Resto = 0;
+
+    if(cpf == "00000000000"){
+        return false;
+    }
+    if(cpf.length != 11){
+        return false;
+    }
+    for (i=1; i<=9; i++){
+        Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+    }
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11)){
+        Resto = 0;
+    } 
+    if (Resto != parseInt(cpf.substring(9, 10))){
+        return false;
+    } 
+
+    Soma = 0;
+    for (i = 1; i <= 10; i++){
+        Soma = Soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
+    }
+
+    Resto = (Soma * 10) % 11;
+      
+    if ((Resto == 10) || (Resto == 11)) {
+        Resto = 0;
+    }
+        
+    if (Resto != parseInt(cpf.substring(10, 11))){
+        return false;
+    }
+       
+    return true;
+}
